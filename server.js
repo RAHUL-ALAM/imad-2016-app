1bdb2e384f2ffa2632d50a68cd56daa61c51f7ec
RAHUL <me15btech11030@iith.ac.in>
2016-11-26 01:44:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/login',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT *  "user" username =$1',[username],function(err,result){
+    pool.query('SELECT *  "user" WHERE username =$1',[username],function(err,result){
         if(err)
         {
             res.status(500).send(err.toString());
