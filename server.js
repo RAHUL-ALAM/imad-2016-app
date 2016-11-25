6a93a9ae9d799d606d0a97543127eac4d1ce04a0
RAHUL <me15btech11030@iith.ac.in>
2016-11-26 01:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,11 +33,22 @@ app.get('/hash:/input',function(req,res){
 app.get('/create-user',function(req,res){
     var salt = crypto.getRandomBytes(128).toString('hex');
     var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2),[username,dbstring])
+    pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbstring],function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send('user successfully created :'+username);
+        }
+    })
 });
 
 var pool = new Pool(config);
-
+app.get('/test-db',function(req,res){
+    
+})
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
